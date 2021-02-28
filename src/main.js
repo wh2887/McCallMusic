3ed863099cd96a4131d458105882be50a03763a5
mccall <1059511459@qq.com>
2021-02-28 23:12:05 +0800

    CORS

---
+++ src/main.js
@@ -0,0 +1,57 @@
+// 事件中心 ： 发布订阅模式 / new CustomEvent
+const EventCenter = {
+  on(type, handler) {
+    $(document).on(type, handler)
+  },
+  fire(type, data) {
+    $(document).trigger(type, data)
+  }
+}
+// 测试可用：
+// EventCenter.on('hello', function(e, data){
+//   console.log(data)
+// })
+// EventCenter.fire('hello', '你好')
+
+
+const Footer = {
+  init() {
+    // 初始化
+
+
+    // 并做绑定事件的操作 以及 第一次渲染页面
+    this.bind()  // 因为要发请求
+    this.render()
+  },
+  bind() {
+    // 绑定事件
+  },
+  render() {
+    // 渲染页面  需要数据，所以先请求数据后渲染
+    // 1. 请求数据
+    const _this = this
+    $.getJSON('http://api.jirengu.com/fm/getSong.php')
+      .done(function(ret){
+        console.log(ret)
+        _this.renderFooter(ret.channels)
+      }).fail(function(){
+      console.log('error')
+    })
+    // axios({
+    //   method: 'get',
+    //   url: 'http://api.jirengu.com/fm/getSong.php',
+    // }).then((response) => {
+    //   console.log('成功获得数据：')
+    //   console.log(response)
+    // })
+    //   .catch((error) => {
+    //     console.log(error)
+    //   })
+    // 2. 渲染 footer
+  },
+  renderFooter() {
+
+  }
+}
+Footer.init()
+
